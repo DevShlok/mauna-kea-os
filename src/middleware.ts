84d915aaf297b58f072007da79bf817d17bd68b5
Shlok Shukla <shlok.shukla@cloudambassadors.com>
2026-06-11 09:47:03 +0530

    Fix middleware auth promise error

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    auth().protect();
+    const authObject = await auth();
+    await authObject.protect();
   }
   return NextResponse.next();
 });
