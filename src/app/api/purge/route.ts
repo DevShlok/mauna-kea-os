34d5c5e02500bc860c0cb0a090386ab992d039dd
Shlok Shukla <shlok.shukla@cloudambassadors.com>
2026-06-11 15:40:23 +0530

    Fix type error in purge route

--- src/app/api/purge/route.ts
+++ src/app/api/purge/route.ts
@@ -1,7 +1,9 @@
-import { revalidateTag } from "next/cache";
+import { revalidateTag, revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  // @ts-ignore
   revalidateTag("dashboard-data");
+  revalidatePath("/", "layout");
   return NextResponse.json({ message: "Vercel data cache purged successfully! The newest database data will now be fetched." });
 }
