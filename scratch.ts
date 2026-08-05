722fd1bd197eb9f7c269f142add70085367014b3
DevShlok <shlokshukla1469@gmail.com>
2026-08-06 01:05:56 +0530

    Cleanup

--- scratch.ts
+++
@@ -1,8 +0,0 @@
-import { mapCandidatesAction } from "./src/actions/candidates";
-
-async function test() {
-  const res = await mapCandidatesAction(["Full Name", "Job Title", "Email Address"], [{"Full Name": "John Doe", "Job Title": "Dev", "Email Address": "john@example.com"}]);
-  console.log(JSON.stringify(res, null, 2));
-}
-
-test();
